ea9af6fb3f82dbea08149a5833b9d5c66ffa3694
Jasminmay16 <150906734+Jasminmay16@users.noreply.github.com>
2023-11-15 12:52:58 +0100

    opgave 3

--- assets/js/functionsThree.js
+++ assets/js/functionsThree.js
@@ -2,16 +2,20 @@
 
 
 // her skal du skrive din anonyme funktion
-let myAnonFunc=
-
+let myAnonFunc= function (){
+    return ("Hello World");
+}
+console.log (myAnonFunc);
 
 // her kaldes, eller invokes din anonyme funktion gennem en variabel
 console.log(myAnonFunc());
 
 
 // her skal du skrive din arrow funktion
-let myArrowFunc=
-
+let myArrowFunc=()=> {
+    return ("Hello World");
+}
+console.log(myArrowFunc());
 
 // her kaldes, eller invokes din arrow funktion gennem en variabel
 console.log(myArrowFunc());
